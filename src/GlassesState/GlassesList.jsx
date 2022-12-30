e841a5a7b72746089544e382639ba7a2261afd6f
haitiano1 <haihoangrr@gmail.com>
2022-12-30 12:41:00 +0700

    bai tap Glass

---
+++ src/GlassesState/GlassesList.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class GlassesList extends Component {
+  renderGlassList = () => {
+    return this.props.glassesList.map((glass) => {
+      return <div className="col-2 mt-2" key={glass.id}>
+      <img onClick={()=>{
+        this.props.showInfo(glass)
+      }} src={glass.url} className="img-fluid" alt="this is image"/>
+      </div>
+    })
+  }
+
+  render() {
+    return (
+      <div className="row">
+                {this.renderGlassList()}
+            </div>
+    )
+  }
+}
